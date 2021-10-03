44471324fc1d7d0ee567c69206edf8c6850cfcf4
Jahan Addison <info@jahanaddison.com>
2021-10-02 22:13:25 -0400

    design form options in gui

--- app/main.cc
+++ app/main.cc
@@ -1,72 +1,69 @@
+/*
+    src/example2.cpp -- C++ version of an example application that shows
+    how to use the form helper class. For a Python implementation, see
+    '../python/example2.py'.
+    NanoGUI was developed by Wenzel Jakob <wenzel.jakob@epfl.ch>.
+    The widget drawing code is based on the NanoVG demo application
+    by Mikko Mononen.
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE.txt file.
+*/
+
 #include <nanogui/nanogui.h>
 #include <iostream>
 
 using namespace nanogui;
 
-enum test_enum {
-    Item1 = 0,
-    Item2,
-    Item3
+
+enum class difficulty {
+    Beginner = 0,
+    Hard,
+    Insane
 };
 
-bool bvar = true;
-int ivar = 12345678;
-double dvar = 3.1415926;
-float fvar = (float)dvar;
-std::string strval = "A string";
-test_enum enumval = Item2;
-Color colval(0.5f, 0.5f, 0.7f, 1.f);
+std::string PLAYER_1 = "Anonymous";
 
-int main(int /* argc */, char ** /* argv */) {
+int main(int argc, char** argv) {
     nanogui::init();
 
+    difficulty default = difficulty::Beginner;
+
     /* scoped variables */ {
-        bool use_gl_4_1 = false;// Set to true to create an OpenGL 4.1 context.
-        Screen *screen = nullptr;
+        bool use_gl_4_1 = true;// Set to true to create an OpenGL 4.1 context.
+        Screen* screen = nullptr;
 
         if (use_gl_4_1) {
             // NanoGUI presents many options for you to utilize at your discretion.
             // See include/nanogui/screen.h for what all of these represent.
-            screen = new Screen(Vector2i(500, 700), "fjórir [GL 4.1]",
-                                /*resizable*/true, /*fullscreen*/false, /*colorBits*/8,
-                                /*alphaBits*/8, /*depthBits*/24, /*stencilBits*/8,
-                                /*nSamples*/0, /*glMajor*/4, /*glMinor*/1);
-        } else {
-            screen = new Screen(Vector2i(500, 700), "fjórir");
+            screen = new Screen(Vector2i(900, 600), "fjorir [GL 4.1]",
+                /* resizable */ false, /* fullscreen */ false,
+                /* depth_buffer */ true, /* stencil_buffer */ true,
+                /* float_buffer */ false, /* gl_major */ 4,
+                /* gl_minor */ 1);
+        }
+        else {
+            screen = new Screen(Vector2i(900, 600), "fjorir");
         }
 
         bool enabled = true;
-        FormHelper *gui = new FormHelper(screen);
-        ref<Window> window = gui->addWindow(Eigen::Vector2i(10, 10), "Form helper example");
-        gui->addGroup("Basic types");
-        gui->addVariable("bool", bvar);
-        gui->addVariable("string", strval);
+        FormHelper* gui = new FormHelper(screen);
+        ref<Window> window = gui->add_window(Vector2i(50, 50), "Let's Play!");
+        gui->add_group("Options");
+        gui->add_button("Reset", []() { std::cout << "Game reset!" << std::endl; });
+        gui->add_button("Invite...", []() { std::cout << "Game reset!" << std::endl; });
+        gui->add_group("Who's Playing");
 
-        gui->addGroup("Validating fields");
-        gui->addVariable("int", ivar)->setSpinnable(true);
-        gui->addVariable("float", fvar);
-        gui->addVariable("double", dvar)->setSpinnable(true);
+        gui->add_variable("Player 1", PLAYER_1)->set_placeholder("(type your name here)");
+        gui->add_variable("Player 2", std::string("AI"));
 
-        gui->addGroup("Complex types");
-        gui->addVariable("Enumeration", enumval, enabled)
-           ->setItems({"Item 1", "Item 2", "Item 3"});
-        gui->addVariable("Color", colval)
-           ->setFinalCallback([](const Color &c) {
-                 std::cout << "ColorPicker Final Callback: ["
-                           << c.r() << ", "
-                           << c.g() << ", "
-                           << c.b() << ", "
-                           << c.w() << "]" << std::endl;
-             });
+        gui->add_variable("Difficulty", default, enabled)
+            ->set_items({ "Beginner", "Hard", "Insane" });
 
-        gui->addGroup("Other widgets");
-        gui->addButton("A button", []() { std::cout << "Button pressed." << std::endl; });
+        screen->set_visible(true);
+        screen->perform_layout();
+        //window->center();
 
-        screen->setVisible(true);
-        screen->performLayout();
-        window->center();
-
-        nanogui::mainloop();
+        nanogui::mainloop(-1);
     }
 
     nanogui::shutdown();
