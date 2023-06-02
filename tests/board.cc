13bdc823dd2feb10af7814a8e9c6a1c539e45d0d
Jahan Addison <jahan@jahanaddison.com>
2023-06-02 03:09:45 -0400

    chore: formatting and clutter

--- tests/board.cc
+++ tests/board.cc
@@ -1,17 +1,6 @@
 /*
-    conect is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    conect is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with conect.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ *   conect is free software under GPL v3 -- see LICENSE for details.
+ */
 
 #include "doctest/doctest.h"
 // #include <board.h>
